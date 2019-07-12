cebb8bfd20a454fddc6f7d4dfaf50cdd232f05b4
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-12 09:08:40 +0100

    Create delete endpoint for projectRouter

--- routes/projects/projectRouter.js
+++ routes/projects/projectRouter.js
@@ -1,51 +1,60 @@
-const express = require('express');
-const Projects = require('../../data/helpers/projectModel')
+const express = require("express");
+const Projects = require("../../data/helpers/projectModel");
 
 const router = express.Router();
 
 // POST
-router.post('/', validateProject, async (req, res) => {
+router.post("/", validateProject, async (req, res) => {
   try {
     const createdProject = await Projects.insert(req.newProject);
     res.status(201).json(createdProject);
   } catch (err) {
-    res.status(500).json({message: err.message})
+    res.status(500).json({ message: err.message });
   }
-})
+});
 
 // GET
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const projects = await Projects.get();
-    res.status(200).json(projects)
+    res.status(200).json(projects);
   } catch (err) {
-    res.status(500).json({message: err.message})
+    res.status(500).json({ message: err.message });
   }
-})
+});
 
-router.get('/:id', validateProjectId, async (req, res) => {
+router.get("/:id", validateProjectId, async (req, res) => {
   try {
     const project = await Projects.get(req.params.id);
-    res.status(200).json(project)
+    res.status(200).json(project);
   } catch (err) {
-    res.status(500).json({message: err.message})
+    res.status(500).json({ message: err.message });
   }
-})
+});
 
 // PUT
-router.put('/:id', validateProjectId, validateProject, async (req, res) => {
+router.put("/:id", validateProjectId, validateProject, async (req, res) => {
   try {
     const updatedProject = await Projects.update(req.params.id, req.newProject);
-    res.status(200).json(updatedProject)
+    res.status(200).json(updatedProject);
   } catch (err) {
-    res.status(500).json({message: err.message})
+    res.status(500).json({ message: err.message });
   }
-})
+});
 
 // DELETE
-router.delete('/:id', validateProjectId, (req, res) => {
-  res.send('api/projects/:id del')
-})
+router.delete("/:id", validateProjectId, async (req, res) => {
+  try {
+    const deleteStatus = await Projects.remove(req.params.id);
+    if (deleteStatus) {
+      res.status(200).json(req.project);
+    } else {
+      res.status(400).json({ message: "The project could not be deleted" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 // validate post middleware
 function validateProject(req, res, next) {
@@ -54,9 +63,11 @@ function validateProject(req, res, next) {
   if (Object.keys(req.body).length === 0) {
     res.status(400).json({ message: "Missing project data" });
   } else if (!name || !description) {
-    res.status(400).json({ message: "Missing required name or description field" });
+    res
+      .status(400)
+      .json({ message: "Missing required name or description field" });
   } else {
-    req.newProject = { name, description }
+    req.newProject = { name, description };
     next();
   }
 }
@@ -72,7 +83,9 @@ async function validateProjectId(req, res, next) {
       } else {
         res
           .status(404)
-          .json({ message: `The project with Id of '${id}' could not be found` });
+          .json({
+            message: `The project with Id of '${id}' could not be found`
+          });
       }
     } catch {
       res.status(500).json({ message: "The project could not be retrieved" });
