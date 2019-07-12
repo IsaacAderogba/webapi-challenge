99c0cf4a37605353fe80761bcb2204464644935b
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-12 09:40:13 +0100

    Tweak port config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const server = require('./server.js');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 server.listen(port, () => {
   console.log(`\n* Server Running on ${port} *\n`)
 })
