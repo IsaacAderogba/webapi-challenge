73007d963823766c6d39e110b3f5d0c35b57b245
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-12 09:37:24 +0100

    Add process.env.PORT to index.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 const projectRouter = require("./routes/projects/projectRouter");
 const actionRouter = require("./routes/actions/actionRouter");
-
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 server.use("/api/projects", projectRouter);
 server.use("/api/actions", actionRouter);
 
